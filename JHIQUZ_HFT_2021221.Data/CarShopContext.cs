f41df64211b113131b66a2b8cdba3aadf0f8fe30
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-10-17 18:50:14 +0200

    context class created

---
+++ JHIQUZ_HFT_2021221.Data/CarShopContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Data
+{
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarShopDatabase.mdf;Integrated Security=True
+    class CarShopContext
+    {
+    }
+}
