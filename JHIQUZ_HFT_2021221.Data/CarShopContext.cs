8d7bffaffffb5a9c39398439cfa95ded98f566b1
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-10-17 19:25:46 +0200

    nugtes installed, CarShopContext properties added

--- JHIQUZ_HFT_2021221.Data/CarShopContext.cs
+++ JHIQUZ_HFT_2021221.Data/CarShopContext.cs
@@ -1,13 +1,21 @@
-﻿using System;
+﻿using JHIQUZ_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace JHIQUZ_HFT_2021221.Data
 {
     //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\CarShopDatabase.mdf;Integrated Security=True
     class CarShopContext
     {
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Car> Cars { get; set; }
+        public virtual DbSet<Engine> Engines { get; set; }
+
+
     }
 }
