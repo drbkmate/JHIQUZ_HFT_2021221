d436f39bb2ed105bcb3f53eba7ac09d01f64399a
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-11-14 20:59:57 +0100

    ICarLogic created

---
+++ JHIQUZ_HFT_2021221.Logic/ICarLogic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Logic
+{
+    public interface ICarLogic
+    {
+        double AveragePrice();
+
+        IEnumerable<KeyValuePair<string, double>>
+            AveragePricesByBrands();
+    }
+}
