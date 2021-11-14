74b176daba4685291ce468da9de75d31aa0123f1
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-11-14 16:09:16 +0100

    CarRepo created, CRUD implemented

---
+++ JHIQUZ_HFT_2021221.Repository/CarRepository.cs
@@ -0,0 +1,56 @@
+﻿using JHIQUZ_HFT_2021221.Data;
+using JHIQUZ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Repository
+{
+    class CarRepository : ICarRepository
+    {
+        CarShopContext context;
+
+        public CarRepository(CarShopContext context)
+        {
+            this.context = context;
+        }
+
+        public void Create(Car car)
+        {
+            context.Cars.Add(car);
+            context.SaveChanges();
+        }
+
+        public void Delete(int carId)
+        {
+            context.Cars.Remove(ReadOne(carId));
+            context.SaveChanges();
+        }
+
+        public IQueryable<Car> ReadAll()
+        {
+            return context.Cars;
+        }
+
+        public Car ReadOne(int id)
+        {
+            return context
+                .Cars
+                .FirstOrDefault(c => c.Id == id);
+        }
+
+        public void Update(Car car)
+        {
+            Car old = ReadOne(car.Id);
+
+            old.Model = car.Model;
+            old.BasePrice = car.BasePrice;
+            old.BrandId = car.BrandId;
+            old.EngineId = car.EngineId;
+            
+            context.SaveChanges();
+        }
+    }
+}
