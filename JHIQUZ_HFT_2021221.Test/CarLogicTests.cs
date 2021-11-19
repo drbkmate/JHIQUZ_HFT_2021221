bf851ba3f840b1b20605fb4493e97fe61fdc6589
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-11-19 15:25:25 +0100

    Added nugets to .Test layer, created CarLogicTests

---
+++ JHIQUZ_HFT_2021221.Test/CarLogicTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Test
+{
+    public class CarLogicTests
+    {
+
+
+    }
+}
