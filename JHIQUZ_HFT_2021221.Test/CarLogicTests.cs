fbd5908aef927d3ab0c2f6903c7b3c564edd5517
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-11-20 13:24:46 +0100

    Added CheckDieselEngineAudies() test method

--- JHIQUZ_HFT_2021221.Test/CarLogicTests.cs
+++ JHIQUZ_HFT_2021221.Test/CarLogicTests.cs
@@ -21,6 +21,11 @@ namespace JHIQUZ_HFT_2021221.Test
             Mock<ICarRepository> mockRepo = new Mock<ICarRepository>();
             Brand b1 = new Brand() { Name = "BMW" };
             Brand b2 = new Brand() { Name = "Audi" };
+            Engine e1 = new Engine() { Ccm = 2000 , Fuel = FuelType.Gasoline};
+            Engine e2 = new Engine() { Ccm = 2200 , Fuel = FuelType.Diesel};
+            Engine e3 = new Engine() { Ccm = 1800 , Fuel = FuelType.Gasoline};
+            Engine e4 = new Engine() { Ccm = 1900 , Fuel = FuelType.Diesel};
+
 
             mockRepo
                 .Setup(x => x.ReadAll())
@@ -28,27 +33,42 @@ namespace JHIQUZ_HFT_2021221.Test
                 {
                     new Car() {
                         BasePrice = 5000,
-                        Brand = b1
+                        Brand = b1,
+                        Engine = e1,
+                        Model = "5 series"
                     },
                     new Car() {
                         BasePrice = 6000,
-                        Brand = b1
+                        Brand = b1,
+                        Engine = e2,
+                        Model = "5 series"
                     },
                     new Car() {
                         BasePrice = 4500,
-                        Brand = b2
+                        Brand = b2,
+                        Engine = e3,
+                        Model = "A4"
                     },
                     new Car() {
                         BasePrice = 7700,
-                        Brand = b2
+                        Brand = b2,
+                        Engine = e4,
+                        Model = "A4"
+
                     },
                     new Car() {
                         BasePrice = 7500,
-                        Brand = b2
+                        Brand = b2,
+                        Engine = e1,
+                        Model = "A4"
+
                     },
                     new Car() {
                         BasePrice = 3200,
-                        Brand = b2
+                        Brand = b2,
+                        Engine = e2,
+                        Model = "A4"
+
                     },
                 }
                 .AsQueryable());
@@ -77,7 +97,29 @@ namespace JHIQUZ_HFT_2021221.Test
             var abp = logic.AverageBmwPrices();
             Assert.That(abp.Where(x => x.Key == "BMW"), Is.EqualTo(abp.Where(x => x.Value == 5500)));
         }
+        [Test]
+        public void CheckMostExpensiveByBrands()
+        { 
+            var mebb = logic.MostExpensiveByBrands();
+            Assert.That(mebb.Where(x => x.Key == "BMW"), Is.EqualTo(mebb.Where(x => x.Value == 6000)));
+            Assert.That(mebb.Where(x => x.Key == "Audi"), Is.EqualTo(mebb.Where(x => x.Value == 7700)));
+        }
+        /*
+        [Test]
+        public void CheckBiggestEnginesByModels()
+        {
+            var bebm = logic.BiggestEnginesByModels();
+            Assert.That(bebm.Where(x => x.Key == "5 series"), Is.EqualTo(bebm.Where(x => x.Value == 2200)));
+            Assert.That(bebm.Where(x => x.Key == "A4"), Is.EqualTo(bebm.Where(x => x.Value == 2200)));
 
-
+        }*/
+        
+        [Test]
+        public void CheckDieselEngineAudies()
+        {
+            var dea = logic.DieselEngineAudies();
+            Assert.That(dea.Where(x => x.Key == "Audi"), Is.EqualTo(dea.Where(x => x.Value == 2200)));
+            Assert.That(dea.Where(x => x.Key == "Audi"), Is.EqualTo(dea.Where(x => x.Value == 1900)));
+        }
     }
 }
