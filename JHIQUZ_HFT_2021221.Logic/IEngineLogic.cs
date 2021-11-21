890654123fc1524be05bcfa9f9f165cda240bf4e
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-11-21 10:35:00 +0100

    IEngineLogic created

---
+++ JHIQUZ_HFT_2021221.Logic/IEngineLogic.cs
@@ -0,0 +1,20 @@
+﻿using JHIQUZ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Logic
+{
+    public interface IEngineLogic
+    {
+        double AverageCcm();
+
+        void Create(Engine car);
+        Engine ReadOne(int id);
+        IQueryable<Engine> ReadAll();
+        void Update(Engine car);
+        void Delete(int engineId);
+    }
+}
