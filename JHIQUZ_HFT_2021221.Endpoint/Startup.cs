163b051bbeb9717de98e3fa1b80220cfa37e1fd3
drbkmate <drabikmate@gmail.com>
2021-11-25 17:10:11 +0100

    .Endpoint set as startup config, Startup's Configure and ConfigureServices modified

--- JHIQUZ_HFT_2021221.Endpoint/Startup.cs
+++ JHIQUZ_HFT_2021221.Endpoint/Startup.cs
@@ -16,6 +16,9 @@ namespace JHIQUZ_HFT_2021221.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            //services.AddTransient();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +28,15 @@ namespace JHIQUZ_HFT_2021221.Endpoint
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers(); //hogy controllereket használjon
             });
         }
     }
