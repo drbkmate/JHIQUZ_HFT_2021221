ed4d9dc0a9f4cdb8b5a9e0769c2c8379d782082f
drbkmate <drabikmate@gmail.com>
2021-11-25 17:13:09 +0100

    Added JsonIgnore to Brand against endless json converting

--- JHIQUZ_HFT_2021221.Models/Brand.cs
+++ JHIQUZ_HFT_2021221.Models/Brand.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace JHIQUZ_HFT_2021221.Models
@@ -20,6 +21,7 @@ namespace JHIQUZ_HFT_2021221.Models
         public string Name { get; set; }
 
         [NotMapped]
+        [JsonIgnore]
         public virtual ICollection<Car> Cars { get; set; }
 
         public Brand()
