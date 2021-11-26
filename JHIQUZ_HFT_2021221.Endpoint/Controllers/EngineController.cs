e87b681278351184780865ba41c6024871f8220b
drbkmate <drabikmate@gmail.com>
2021-11-26 23:30:20 +0100

    EngineController created and added CRUD methods

---
+++ JHIQUZ_HFT_2021221.Endpoint/Controllers/EngineController.cs
@@ -0,0 +1,52 @@
+﻿using JHIQUZ_HFT_2021221.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Logic
+{
+    [Route("[controller")]
+    [ApiController]
+    public class EngineController : ControllerBase
+    {
+        private IEngineLogic logic;
+
+        public EngineController(IEngineLogic logic)
+        {
+            this.logic = logic;
+        }
+        //OK
+        // URL: /engine
+        [HttpGet]
+        public IEnumerable<Engine> ReadAllEngines()
+        {
+            return logic.ReadAll();
+        }
+
+        //
+        // URL: /engine
+        [HttpPost]
+        public void CreateEngine(Engine engine)
+        {
+            logic.Create(engine);
+        }
+
+        //OK    
+        //URL: /engine/{carId}
+        [HttpDelete("{engineId}")]
+        public void DeleteEngine([FromRoute] int engineId)
+        {
+            logic.Delete(engineId);
+        }
+
+        //OK
+        [HttpPut]
+        public void UpdateEngine([FromBody] Engine engine)
+        {
+            logic.Update(engine);
+        }
+    }
+}
