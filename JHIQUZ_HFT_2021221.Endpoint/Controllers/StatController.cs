80d81278e7ba385df900853694b9bb91a4a8f1c2
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-12-02 18:32:39 +0100

    Added StatController for ICarLogic's non-crud methods

---
+++ JHIQUZ_HFT_2021221.Endpoint/Controllers/StatController.cs
@@ -0,0 +1,57 @@
+﻿using JHIQUZ_HFT_2021221.Logic;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Endpoint.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class StatController : ControllerBase
+    {
+        private ICarLogic logic;
+
+        public StatController(ICarLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet] //stat/avgprice
+        public double GetAvgPrice()
+        {
+            return logic.AveragePrice();
+        }
+
+        [HttpGet] //stat/avgpricesbybrands
+        public IEnumerable<KeyValuePair<string,double>> GetAvgPricesByBrands()
+        {
+            return logic.AveragePricesByBrands();
+        }
+
+        [HttpGet] //stat/avgbmwprices
+        public IEnumerable<KeyValuePair<string, double>> GetAverageBmwPrices()
+        {
+            return logic.AverageBmwPrices();
+        }
+
+        [HttpGet] //stat/mostexpensivebybrands
+        public IEnumerable<KeyValuePair<string, int>> GetMostExpensiveByBrands()
+        {
+            return logic.MostExpensiveByBrands();
+        }
+
+        [HttpGet] //stat/biggestenginesbymodels
+        public IEnumerable<KeyValuePair<string, int>> GetBiggestEnginesByModels()
+        {
+            return logic.BiggestEnginesByModels();
+        }
+
+        [HttpGet] //stat/avgccmdieselengineaudies
+        public IEnumerable<KeyValuePair<string, double>> GetAvgCcmDieselEngineAudies()
+        {
+            return logic.AvgCcmDieselEngineAudies();
+        }
+    }
+}
