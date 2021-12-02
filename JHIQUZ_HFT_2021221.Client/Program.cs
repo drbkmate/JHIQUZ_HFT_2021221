acab162db5ff10b9fd5973155f5eb4ff0a706878
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-12-02 19:22:22 +0100

    Deleted data include from .Client, RestService test in Program.cs

--- JHIQUZ_HFT_2021221.Client/Program.cs
+++ JHIQUZ_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using JHIQUZ_HFT_2021221.Data;
+﻿using HFT_FF_L08.Client;
+using JHIQUZ_HFT_2021221.Models;
 using System;
 using System.Linq;
 
@@ -8,11 +9,23 @@ namespace JHIQUZ_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
+            /*
             CarShopContext ctx = new CarShopContext();
 
             var res1 = ctx.Cars.ToList();
 
+            ;*/
+
+            System.Threading.Thread.Sleep(8000);
+
+            RestService rest = new RestService("http://localhost:51322");
+
+            var cars = rest.Get<Car>("car");
+            var brands = rest.Get<Car>("brand");
+            var engines = rest.Get<Car>("engine");
+
             ;
+
         }
     }
 }
