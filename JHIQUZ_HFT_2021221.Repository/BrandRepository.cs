d801d3caf043a3778078695a78cca681b26ea449
drbkmate <drabikmate@stud.uni-obuda.hu>
2021-12-02 19:06:10 +0100

    Created Interface for BrandRepository

--- JHIQUZ_HFT_2021221.Repository/BrandRepository.cs
+++ JHIQUZ_HFT_2021221.Repository/BrandRepository.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace JHIQUZ_HFT_2021221.Repository
 {
-    public class BrandRepository
+    public class BrandRepository : IBrandRepository
     {
         CarShopContext context;
 
---
+++ JHIQUZ_HFT_2021221.Repository/IBrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using JHIQUZ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.Repository
+{
+    public interface IBrandRepository
+    {
+        void Create(Brand brand);
+        Brand ReadOne(int id);
+        IQueryable<Brand> ReadAll();
+        void Update(Brand brand);
+        void Delete(int brandId);
+    }
+}
