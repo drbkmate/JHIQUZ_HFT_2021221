2f7dcdace9586cb57ef6bb0c77bd4b6e8f3dab34
drbkmate <92025749+drbkmate@users.noreply.github.com>
2022-04-27 22:00:41 +0200

    added WpfClient

---
+++ JHIQUZ_HFT_2021221.WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using JHIQUZ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JHIQUZ_HFT_2021221.WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Car> Cars { get; set; }
+        public MainWindowViewModel()
+        {
+            Cars = new RestCollection<Car>("http://localhost:51322/", "car");
+        }
+    }
+}
