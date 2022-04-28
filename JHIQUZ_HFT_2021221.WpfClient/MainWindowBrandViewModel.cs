8d440bb1daea295e00b2d0931cd68fa70684cb71
drbkmate <92025749+drbkmate@users.noreply.github.com>
2022-04-28 15:32:00 +0200

    Created ViewModel for BrandPage

---
+++ JHIQUZ_HFT_2021221.WpfClient/MainWindowBrandViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using JHIQUZ_HFT_2021221.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace JHIQUZ_HFT_2021221.WpfClient
+{
+    public class MainWindowBrandViewModel : ObservableRecipient
+    {
+        public RestCollection<Brand> Brands { get; set; }
+        private Brand selectedBrand;
+
+        public Brand SelectedBrand
+        {
+            get { return selectedBrand; }
+            set
+            {
+                if (value != null)
+                {
+                    selectedBrand = new Brand()
+                    {
+                        Name = value.Name
+
+                    };
+                    OnPropertyChanged();
+                    (DeleteBrandCommand as RelayCommand).NotifyCanExecuteChanged();
+                }
+
+            }
+        }
+
+        public ICommand CreateBrandCommand { get; set; }
+        public ICommand DeleteBrandCommand { get; set; }
+        public ICommand UpdateBrandCommand { get; set; }
+        public MainWindowBrandViewModel()
+        {
+
+            Brands = new RestCollection<Brand>("http://localhost:51322/", "brand", "hub"); //BRAND HUB?
+            CreateBrandCommand = new RelayCommand(() =>
+            {
+                Brands.Add(new Brand()
+                {
+                    Name = "Misubishi"
+                });
+            }
+            );
+
+            UpdateBrandCommand = new RelayCommand(() =>
+            {
+                Brands.Update(SelectedBrand);
+            });
+
+            DeleteBrandCommand = new RelayCommand(
+            () =>
+            {
+                Brands.Delete(SelectedBrand.Id);
+            },
+            () =>
+            {
+                return SelectedBrand != null;
+            }
+            );
+            SelectedBrand = new Brand(); //nem dob null ref. exceptiont ha nincs kiválasztva semmi
+        }
+    }
+}
