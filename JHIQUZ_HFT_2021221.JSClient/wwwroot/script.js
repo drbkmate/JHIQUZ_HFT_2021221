9b469301a323c32717aa5acf5d0f0242080f4403
drbkmate <92025749+drbkmate@users.noreply.github.com>
2022-04-30 22:35:28 +0200

    Added update() function, created updateformdiv html element

--- JHIQUZ_HFT_2021221.JSClient/wwwroot/script.js
+++ JHIQUZ_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,17 +1,31 @@
 ﻿let cars = [];
-const connection;
+let connection = null;
+let carIdToUpdate = -1;
+
 getdata();
+setupSignalR();
 
-
-function setup SignalR(){
+function setupSignalR() {
+    //kapcsolódik
     connection = new signalR.HubConnectionBuilder()
         .withUrl("http://localhost:51322/hub")
         .configureLogging(signalR.LogLevel.Information)
         .build();
+    //feliratkoz
     connection.on("CarCreated", (user, message) => {
-        console.log(user);
-        console.log(message);
+        getdata();
     });
+    connection.on("CarDeleted", (user, message) => {
+        getdata();
+    });
+    connection.on("CarUpdated", (user, message) => {
+        getdata();
+    });
+    //elindít
+    connection.onclose(async () => {
+        await start();
+    });
+    start();
 }
 async function start() {
     try {
@@ -28,7 +42,7 @@ async function getdata() {
         .then(x => x.json())
         .then(y => {
             cars = y
-            console.log(cars);
+            //console.log(cars);
             display();
         });
 }
@@ -37,7 +51,7 @@ function display() {
     document.getElementById('resultArea').innerHTML = "";
     cars.forEach(t => {
         document.getElementById('resultArea').innerHTML +=
-            "<tr><td>" + t.id + "</td><td>" + t.brand.name + "</td><td>" + t.model + "</td><td>" + t.basePrice + "</td><td>" + t.engine.ccm + "</td><td>" + `<button type="button" class="btn btn-danger" onclick="remove(${t.id})">Delete</button>` + "</td></tr>";
+            "<tr><td>" + t.id + "</td><td>" + t.brand.name + "</td><td>" + t.model + "</td><td>" + t.basePrice + "</td><td>" + t.engine.ccm + "</td><td>" + `<button type="button" class="btn btn-danger" onclick="remove(${t.id})">Delete</button>` + `<button type="button" class="btn mx-1 btn-warning" onclick="showupdate(${t.id})">Update</button>` + "</td></tr>";
         console.log(t.model)
     });
 }
@@ -69,6 +83,41 @@ function remove(id) {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json', },
         body: null
+    })
+        .then(response => response)
+        .then(data => { console.log('Success: ', data); getdata(); })
+        .catch((error) => { console.error('Error: ', error); });
+
+}
+
+function showupdate(id) {
+    document.getElementById('model_toupdate').value = cars.find(t => t['id'] == id)['model'];
+    document.getElementById('baseprice_toupdate').value = cars.find(t => t['id'] == id)['basePrice'];
+    document.getElementById('engineid_toupdate').value = cars.find(t => t['id'] == id)['engineId'];
+
+    document.getElementById('updateformdiv').className = 'my-5';
+
+    carIdToUpdate = id;
+    console.log("actoridtoupdate: " + actorIdToUpdate);
+}
+
+function update() {
+    document.getElementById('updateformdiv').className = 'my-5 d-none'
+    let model = document.getElementById('model_toupdate').value;
+    let brandid = document.getElementById('brandid_toupdate').value;
+    let baseprice = document.getElementById('baseprice_toupdate').value;
+    let engineid = document.getElementById('engineid_toupdate').value;
+
+    fetch('http://localhost:51322/car', {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json', },
+        body: JSON.stringify({
+            Id: carIdToUpdate,
+            model: model,
+            brandId: brandid,
+            engineId: engineid,
+            basePrice: baseprice
+        }),
     })
         .then(response => response)
         .then(data => { console.log('Success: ', data); getdata(); })
