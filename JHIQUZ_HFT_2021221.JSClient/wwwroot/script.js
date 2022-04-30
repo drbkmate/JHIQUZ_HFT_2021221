23eff2a6e963122bde1401d22effd5554e1c0f5a
drbkmate <92025749+drbkmate@users.noreply.github.com>
2022-04-30 18:53:58 +0200

    Added table to index.html, added display func. to script

--- JHIQUZ_HFT_2021221.JSClient/wwwroot/script.js
+++ JHIQUZ_HFT_2021221.JSClient/wwwroot/script.js
@@ -1,3 +1,16 @@
-﻿fetch('http://localhost:51322/car')
+﻿let cars = [];
+fetch('http://localhost:51322/car')
     .then(x => x.json())
-    .then(y => console.log(y));
+    .then(y => {
+        cars = y
+        console.log(cars);
+        display();
+    });
+
+
+
+function display() {
+    cars.forEach(t => {
+        console.log(t.Model)
+    });
+}
